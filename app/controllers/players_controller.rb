703ba1c8b7f5fe4a019aa3f3469a067863dbf5dc
AgalanC <galanc.armando@gmail.com>
2024-06-28 00:42:06 -0500

    Added a hierarchical search.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -8,10 +8,31 @@
 #   end
 # end
 
+#  Simple Search
+# class PlayersController < ApplicationController
+#   def index
+#     if params[:search].present?
+#       @players = Player.where("player_name LIKE ?", "%#{params[:search]}%").page(params[:page]).per(10)
+#     else
+#       @players = Player.page(params[:page]).per(10)
+#     end
+#   end
+
+#   def show
+#     @player = Player.find(params[:id])
+#   end
+# end
+
+# Hierarchical Search
 class PlayersController < ApplicationController
   def index
-    if params[:search].present?
+    @teams = Team.all
+    if params[:search].present? && params[:team_id].present?
+      @players = Player.where("player_name LIKE ?", "%#{params[:search]}%").where(team_id: params[:team_id]).page(params[:page]).per(10)
+    elsif params[:search].present?
       @players = Player.where("player_name LIKE ?", "%#{params[:search]}%").page(params[:page]).per(10)
+    elsif params[:team_id].present?
+      @players = Player.where(team_id: params[:team_id]).page(params[:page]).per(10)
     else
       @players = Player.page(params[:page]).per(10)
     end
--- app/controllers/trophies_controller.rb
+++ app/controllers/trophies_controller.rb
@@ -8,10 +8,30 @@
 #   end
 # end
 
+# Simple Search
+# class TrophiesController < ApplicationController
+#   def index
+#     if params[:search].present?
+#       @trophies = Trophy.where("trophy_name LIKE ?", "%#{params[:search]}%").page(params[:page]).per(10)
+#     else
+#       @trophies = Trophy.page(params[:page]).per(10)
+#     end
+#   end
+
+#   def show
+#     @trophy = Trophy.find(params[:id])
+#   end
+# end
+
+# Hierarchical Search
 class TrophiesController < ApplicationController
   def index
-    if params[:search].present?
+    if params[:search].present? && params[:year].present?
+      @trophies = Trophy.where("trophy_name LIKE ?", "%#{params[:search]}%").where(year: params[:year]).page(params[:page]).per(10)
+    elsif params[:search].present?
       @trophies = Trophy.where("trophy_name LIKE ?", "%#{params[:search]}%").page(params[:page]).per(10)
+    elsif params[:year].present?
+      @trophies = Trophy.where(year: params[:year]).page(params[:page]).per(10)
     else
       @trophies = Trophy.page(params[:page]).per(10)
     end
