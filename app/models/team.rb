93c696c8720aca4cd7d5c5dfad839f9b87d86f66
AgalanC <galanc.armando@gmail.com>
2024-07-05 05:43:03 -0500

    Completed features 1.2, 5.2, and 7.2

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,6 +5,9 @@ class Team < ApplicationRecord
   validates :team_name, presence: true
   validates :coach, presence: true
 
+  # Active Storage associations
+  has_one_attached :image
+
   def self.ransackable_associations(auth_object = nil)
     ["players", "trophies"]
   end
